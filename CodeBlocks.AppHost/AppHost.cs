e34fcc12045184bac0f1590648e9f29eca2853dd
evptt <aleksandrshahmatov9@gmail.com>
2026-02-18 11:35:11 +0700

    Delete CodeBlocks.AppHost directory

--- CodeBlocks.AppHost/AppHost.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var server = builder.AddProject<Projects.CodeBlocks_Server>("codeblocks-server");
-
-builder.AddExecutable("codeblocks-client", "npm", "../codeblocks.client", "run", "dev")
-    .WithEnvironment("services__codeblocks-server__https__0", server.GetEndpoint("https"))
-    .WithHttpEndpoint(port: 62408);
-
-builder.Build().Run();
