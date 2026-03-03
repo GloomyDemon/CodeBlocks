83d809bd35e482725a9cc91198f482ac164a5eb1
GloomyDemon <marat.mustafin.06@gmail.com>
2026-03-03 18:41:02 +0700

    Sort methods for clear view

--- Blocks/Blocks/Variable.ts
+++ Blocks/Blocks/Variable.ts
@@ -2,29 +2,30 @@
 import type { INamed } from "../Interfaces/INamed.js";
 
 export class Variable<T> implements INamed, IEvaluable<T> {
+
     readonly #id: string;
+    get id(): string {
+        return this.#id;
+    }
+    
     readonly #name: string;
+    get name(): string {
+        return this.#name;
+    }
+
     #value: T;
+    evaluate(): T {
+        return this.#value;
+    }
+    
+    change(value: T): void {
+        this.#value = value;
+    }
 
     constructor(id: string, name: string, value: T) {
         this.#id = id;
         this.#name = name;
         this.#value = value;
     }
-
-    get id(): string {
-        return this.#id;
-    }
-
-    get name(): string {
-        return this.#name;
-    }
-
-    evaluate(): T {
-        return this.#value;
-    }
-
-    change(value: T): void {
-        this.#value = value;
-    }
+    
 }
