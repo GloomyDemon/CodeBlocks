df1295297ae8452f3fc129fd6dd9a8b489267638
GloomyDemon <marat.mustafin.06@gmail.com>
2026-03-11 04:35:59 +0700

    First commit

---
+++ Pizza_OOP/Classes/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pizza_OOP.Interface;
+
+namespace Pizza_OOP.Classes
+{
+    internal class Pizza: IElement
+    {
+    }
+}
---
+++ Pizza_OOP/Interface/IElement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza_OOP.Interface
+{
+    internal interface IElement
+    {
+    }
+}
